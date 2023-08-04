333819c1cbce96f2a1a0ce7851b5fe69cb47cb9f
FSFbot <felipe.sfreitas14@gmail.com>
2023-08-04 14:07:53 -0300

    During this fase impments exceptions at teste

---
+++ Exeception/Exeception/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Exeception.Entities.Exceptions
+{
+   class DomainException : ApplicationException
+    {
+        public DomainException(string message): base(message)
+        {
+
+        }
+    }
+}
