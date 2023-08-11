2c080d3354a802a010c8c4077f4f352810c01031
FSFbot <felipe.sfreitas14@gmail.com>
2023-08-11 11:08:08 -0300

    there is my new application where is possible making create students and insert notes about your skills and finaly calculated the mean

---
+++ Alunos/Alunos/Entities/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alunos.Entities
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public List<double> Notas { get; set; }
+
+        public double Media()
+        {
+            double soma = 0;
+            foreach(double nota in Notas)
+            {
+                soma += nota;
+            }
+            return soma / Notas.Count;
+        }
+    }
+}
+
---
+++ Alunos/Alunos/Program.cs
@@ -0,0 +1,35 @@
+﻿using Alunos.Entities;
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero de alunos");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Aluno> alunos = new List<Aluno>();
+            
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Digite o nome do aluno {i + 1}:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Quantas notas o lunos {nome} possui?");
+            int numNotas = int.Parse(Console.ReadLine());
+
+            List<double> notas = new List<double>();
+            for(int j = 0; j  < numNotas; j++)
+            {
+                Console.WriteLine($"Digite a nota {j + 1} do aluno {nome}: ");
+                double nota = double .Parse(Console.ReadLine());    
+                notas.Add(nota);
+            }
+            alunos.Add(new Aluno { Nome = nome, Notas = notas });
+        }
+        Console.WriteLine("\nResultados: ");
+        foreach(var aluno in alunos)
+        {
+            Console.WriteLine($"O aluno {aluno.Nome} tem uma média de {aluno.Media():F2}.");
+        }
+    }
+    }
+
